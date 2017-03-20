9efef1c94f62f3084b0816637dd283cc5c5a7463
Sierra 4 <sierra4@OSX22.local>
2017-03-20 16:59:56 +0530

    Initial Commit

---
+++ ChattingApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChattingApplication
+//
+//  Created by Sierra 4 on 20/03/17.
+//  Copyright © 2017 codebrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
